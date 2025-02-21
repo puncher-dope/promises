834402c9761ab172016ebeb8b73feff2ce15375f
Kostya <kimkonstantin2118@gmail.com>
2025-02-21 22:58:17 +0300

    new lesson ASYNC AWAIT

--- Promises.js
+++ Promises.js
@@ -1,71 +1,6 @@
-// console.log('Request data...');
-// //     setTimeout(() => {
-// //         console.log('Preparing data...');
-// //         const backendData = {
-// //             server: 'aws',
-// //             port: 2000,
-// //             status:'working'
-// //         }
-// //             setTimeout(() => {
-// //                     backendData.modifide = true
-// //                     console.log('Data received:', backendData);
-// //             }, 2000)
-// //     }, 2000);
-
-
-// // const p = new Promise((resolve, reject)=> {
-// //     setTimeout(() => {
-// //         console.log('Preparing data...');
-// //                 const backendData = {
-// //                     server: 'aws',
-// //                     port: 2000,
-// //                     status:'working'
-// //                 }
-// //                 resolve(backendData)
-// //     }, 2000)
-// // })
-// // p.then(data => {
-// //     return new Promise((resolve, reject) => {
-// //         setTimeout(() => {
-// //             data.modified = true;
-// //             resolve(data)//resolve-успех, reject -не успех, его словит ошибка
-// //         }, 2000);
-// //     })
-// // })
-// // .then(clientData => {
-// //     console.log('Data received', clientData);
-// //     clientData.fromPromise = true
-// // })
-// // .catch(err => console.error('Error: ', err))//catch ловит reject -ошибки
-// // .finally(() => console.log('Finally'))//это вводится независимо от ошибок
-
-
-// // const sleep = ms => new Promise(resolve => 
-// //     setTimeout(() => {
-// //     resolve()
-// // }, ms))
-// const sleep = ms => new Promise(resolve => {
-//     setTimeout(() => resolve(), ms)
-// })
-
-// sleep(2000).then(() => console.log('After two seconds...'))
-// sleep(4000).then(() => console.log('After four seconds...'))
-
-// Promise.all([sleep(2000), sleep(5000)])//.all выдает последний промис из масива промисов
-// .then(() => {
-//     console.log('All promises');
-// })
-
-// Promise.race([[sleep(2000), sleep(5000)]])// .race выдает первый промис из массива промисов
-// .then(() => {
-//     console.log('Race promises');//консолим всё в GIT
-// })
-//------------------------------------------------------------------------------------------
-
-
 //Повторяем промисы 
 
-console.log('Request data...');
+// console.log('Request data...');
 
 // setTimeout(() => {
 //     console.log('Preparing data...');
@@ -158,11 +93,54 @@ console.log('Request data...');
 
 //---------------------------------------------------Тут просто из урока функция
 
-const sleep = ms => new Promise((resolve) => {
-    setTimeout(() => {
-        resolve()
-    }, ms)
-})
+// const sleep = ms => new Promise((resolve) => {
+//     setTimeout(() => {
+//         resolve()
+//     }, ms)
+// })
 
-sleep(2000).then(() => console.log('after 2 sec'))
-sleep(3000).then(() => console.log('after 3 sec'))
+// sleep(2000).then(() => console.log('after 2 sec'))
+// sleep(3000).then(() => console.log('after 3 sec'))
+//=======================================================Async-await===============================================================
+const delay = ms => new Promise((r) => setTimeout(() =>r(), ms))
+
+// delay(2000).then(() => console.log('2 sec'))
+
+const url ='https://dummyjson.com/products'
+
+// function fetchTodos(){
+//     console.log('Fetch todo started...');
+//    return delay(2000)
+//    .then(() =>  fetch(url))
+//    .then((respons) => respons.json())
+// }
+
+// fetchTodos()
+// .then(data => {
+//     console.log('Data' , data);
+// })
+// .catch(err => console.error(e))
+//========================================делаем ту жу функцию, но с async await
+// async function fetchAsyncTodos(){
+//     console.log('Fetch asyncTodo started...');
+
+//     try {
+//         await delay(2000)
+//         const respons = await fetch(url)
+//         const data = await respons.json()
+//         console.log(data);
+//         throw new Error(respons);
+//     } catch (e) {
+//         console.log('Invalid url:',e);
+//     }finally{
+//         await delay(2000)
+//         console.log('Sucsess')
+//     }
+
+// }
+
+// fetchAsyncTodos()
+// .then(() => delay(2000))
+// .then(() => console.log('fvhbfv'))
+
+
